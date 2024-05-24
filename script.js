ea8dd343fc76ea41ad5f1636ab06a1f12fb49172
Igor Araujo de Mattos <yigor88mattos@gmail.com>
2024-05-24 15:21:09 -0300

    Conserta a data

--- script.js
+++ script.js
@@ -25,11 +25,18 @@ list.addEventListener('click', function (ev) {
 function addElemento() {
     let li = document.createElement("li");
     let inputValue = document.getElementById("tarefa").value;
-    let t = document.createTextNode(inputValue);
+    
+
+    const dataAtual = new Date();
+    const dia = dataAtual.getDate(); // Dia do mês (1-31)
+    const mes = dataAtual.getMonth(); // Mês (0-11, janeiro é 0)
+    const ano = dataAtual.getFullYear();
+    const data = `${dia}/${mes + 1}/${ano} - `;
+
+    let t = document.createTextNode(data + inputValue);
     li.appendChild(t);
     li.classList.add("novo-item");
     li.style.fontFamily = 'Arial';
-
     if (inputValue === '') {
         alert("Você precisa descrever a tarefa");
     } else {
