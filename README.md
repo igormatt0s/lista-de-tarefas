35d584283515bd8e971cdcb578ef201a677bd9e4
Igor Araujo de Mattos <yigor88mattos@gmail.com>
2024-05-30 01:06:48 -0300

    Personaliza a página

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# lista-de-tarefas
+# Lista de Tarefas
+
+Atividade de JavaScript de uma Lista de Tarefas para a disciplina de EC47C - Programação Web Front-End - C71 da professora Rosangela Marquesone, realizado pelos alunos Livia Pereira Garcia, Luiza Kouketsu da Silva e Igor Araujo de Mattos.
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myNodelist = document.getElementsByTagName("li");
 for (let i = 0; i < myNodelist.length; i++) {
     let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7"); //caracter x
+    let txt = document.createTextNode("X"); //caracter x
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
@@ -29,9 +29,13 @@ function addElemento() {
 
     const dataAtual = new Date();
     const dia = dataAtual.getDate(); // Dia do mês (1-31)
-    const mes = dataAtual.getMonth(); // Mês (0-11, janeiro é 0)
+    const mes = dataAtual.getMonth() + 1; // Mês (0-11, janeiro é 0, então somamos 1)
     const ano = dataAtual.getFullYear();
-    const data = `${dia}/${mes + 1}/${ano} - `;
+
+    const diaFormatado = dia.toString().padStart(2, '0'); // Garantir que o dia tenha 2 dígitos
+    const mesFormatado = mes.toString().padStart(2, '0'); // Garantir que o dia tenha 2 dígitos
+
+    const data = `${diaFormatado}/${mesFormatado}/${ano} - `;
 
     let t = document.createTextNode(data + inputValue);
     li.appendChild(t);
@@ -44,7 +48,7 @@ function addElemento() {
     }
     document.getElementById("tarefa").value = "";
     let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
+    let txt = document.createTextNode("X");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
@@ -53,6 +57,12 @@ function addElemento() {
             let div = this.parentElement;
             div.style.display = "none";
         }
+    }
+}
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        addElemento();
     }
 }
 
